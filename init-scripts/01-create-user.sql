e7cdcb12e09e7cb519cf4e7cc2331a38a4360a19
prathamesh <prathameshinoffice@gmail.com>
2025-10-15 19:29:22 +0530

    removed hard coded password

--- init-scripts/01-create-user.sql
+++ init-scripts/01-create-user.sql
@@ -1,4 +1,4 @@
 -- runs automatically on first start
-CREATE USER IF NOT EXISTS 'demo-user'@'%' IDENTIFIED WITH caching_sha2_password BY 'D3m0P@ss!';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX ON mydb.* TO 'demo-user'@'%';
+CREATE USER IF NOT EXISTS 'demo-user'@'%' IDENTIFIED WITH caching_sha2_password BY '${DB_PASSWORD}';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX ON ${DB_NAME}.* TO 'demo-user'@'%';
 FLUSH PRIVILEGES;
